d8da9b358c9e920f5e6fa6a1c1072184c9275065
Krinal <krinalnakarani1774@gmail.com>
2022-12-28 16:25:07 +0530

    28-12

--- lib/utils/firebase.dart
+++ lib/utils/firebase.dart
@@ -1,16 +1,22 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 
+void insertdata(String note) {
+  FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  CollectionReference collectionReference = firebaseFirestore.collection(
+      "note");
 
-void insertdata(String note)
+  collectionReference.add({"note": "$note"})
+      .then((value) => print("Success"))
+      .catchError((error) => print("$error"));
+}
+
+Stream<QuerySnapshot<Map<String, dynamic>>> readdata()
 {
   FirebaseFirestore firebaseFirestore =FirebaseFirestore.instance;
-  CollectionReference collectionReference = firebaseFirestore.collection("note");
-
-  collectionReference.add({"note": "$note"}).then((value) => print("Success")).catchError((error) => print("$error"));
+  return firebaseFirestore.collection("note").snapshots();
 }
+
 
 
 
